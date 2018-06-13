7d05f668fd5d8938fc9f26a404a34ec923cf7ff0
Maurizio Lupo <maurizio.lupo@gmail.com>
2018-06-13 12:35:27 +0100

    bug fixes

--- bin/iter-duct.js
+++ bin/iter-duct.js
@@ -7,9 +7,9 @@ const it = require('iter-tools/es2018')
 const pipelineName = argv.pipeline || 'pipeline'
 const configFile = argv.config || 'iter-duct.config.js'
 
-const iterable iterDuct({ pipelineName, configFile })
+const iterable = iterDuct({ pipelineName, configFile })
 
-it.asyncConsume(iterable)
+const x = it.asyncConsume(() => {}, iterable)
   .catch(err => {
     console.error(err)
     process.exit(1)
